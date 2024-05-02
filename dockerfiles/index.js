37862313a6a497d38d990423aa3910595306b4dc
Marco Oliveira <97900963+marcooli@users.noreply.github.com>
2024-05-02 10:26:07 -0400

    add node

---
+++ dockerfiles/index.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+  app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Dockerizing Node Application"));
+
+app.listen(8080, () =>
+  console.log(`⚡️[bootup]: Server is running at port: 8080`)
+);
